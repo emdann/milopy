f76a909a0c7fcdb2fe1c023b5d2e43cba815e433
Emma Dann <32264060+emdann@users.noreply.github.com>
2022-08-18 11:32:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='milopy',
-      version='0.1.00',
+      version='0.1.0',
       description='python implementation of miloR for differential abundance analysis in single-cell datasets',
       url='https://github.com/emdann/milopy',
       author='Emma Dann',
